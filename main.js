63d6f708c4e1029c82dde94d174dce1ef44b37a9
Roman K <39156575+RV87K@users.noreply.github.com>
2022-12-16 14:57:37 +1000

    feat: add block 'about-myself'

---
+++ main.js
@@ -0,0 +1,9 @@
+const block = document.querySelector('.about-myself')
+const cvTitle = document.querySelector('.cv-title')
+
+
+cvTitle.addEventListener('click', function() {
+  block.classList.add('show-block');
+})
+console.log(cvTitle)
+
